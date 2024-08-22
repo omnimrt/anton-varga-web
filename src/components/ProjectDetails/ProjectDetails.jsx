b206563bf926fcf29b6079ce3e798242fb39ac17
Martin Myhalyna <martinmyhalyna@gmail.com>
2024-08-22 10:50:32 +0200

    fixed margin-right for ProductDetails view

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -1,18 +1,18 @@
-import { CCarousel, CCarouselItem, CImage } from "@coreui/react";
-import "@coreui/coreui/dist/css/coreui.min.css";
-import css from "./ProjectDetails.module.css";
+import { CCarousel, CCarouselItem, CImage } from '@coreui/react';
+import '@coreui/coreui/dist/css/coreui.min.css';
+import css from './ProjectDetails.module.css';
 
 const ProjectDetails = ({ selectedProject, selectedImage }) => {
-  console.log("Selected Project:", selectedProject);
-  console.log("Selected Image:", selectedImage);
+  console.log('Selected Project:', selectedProject);
+  console.log('Selected Image:', selectedImage);
 
   // Find the index of the selected image in the images array
   const selectedIndex = selectedProject.images.findIndex(
-    (image) => image.id === selectedImage?.id
+    image => image.id === selectedImage?.id
   );
 
   return (
-    <div>
+    <div className={css.container}>
       <CCarousel
         className={css.carouselContainer}
         controls
@@ -20,7 +20,7 @@ const ProjectDetails = ({ selectedProject, selectedImage }) => {
         transition="crossfade"
         activeIndex={selectedIndex}
       >
-        {selectedProject.images.map((image) => (
+        {selectedProject.images.map(image => (
           <CCarouselItem key={image.id}>
             <CImage
               className="d-block w-100"
@@ -36,7 +36,19 @@ const ProjectDetails = ({ selectedProject, selectedImage }) => {
           <p className={css.category}>I60 x 48 inch, canvas, oil, 2020</p>
         </div>
         <p className={css.text}>
-        One of the main differences in the landscapes is at the end of the world the bodies are just laying on the ground under the radioactive light while at the End of History, somebody eventually will bury them to hide the evidence or to keep going with everyday life. Since the new phase of Russian aggression in Ukraine started, it was hard to work again on the same dystopian landscapes, mainly because current events produce their own live-streamed versions of last, final, apocalyptic visuals. The streets of Bucha, Irpin, Mariupol, and cities with the victims of the massacres laying about and bodies being buried in mass graves forced me to reassess the idea of the final landscape again. Being wrecked and also confused about this practice and next steps, I stopped working on this series, rethinking the joke in new historical realms and its “never again” status.
+          One of the main differences in the landscapes is at the end of the
+          world the bodies are just laying on the ground under the radioactive
+          light while at the End of History, somebody eventually will bury them
+          to hide the evidence or to keep going with everyday life. Since the
+          new phase of Russian aggression in Ukraine started, it was hard to
+          work again on the same dystopian landscapes, mainly because current
+          events produce their own live-streamed versions of last, final,
+          apocalyptic visuals. The streets of Bucha, Irpin, Mariupol, and cities
+          with the victims of the massacres laying about and bodies being buried
+          in mass graves forced me to reassess the idea of the final landscape
+          again. Being wrecked and also confused about this practice and next
+          steps, I stopped working on this series, rethinking the joke in new
+          historical realms and its “never again” status.
         </p>
       </div>
     </div>
